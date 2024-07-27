917808a6607249e6164e3e540d50985986904fa7
Edwin Sadie <102459003+sadieedwin@users.noreply.github.com>
2024-07-27 12:10:53 +0800

    Update HourlyAlarmV2.bat

--- HourlyAlarmV2.bat
+++ HourlyAlarmV2.bat
@@ -4,6 +4,6 @@ rem Play the sound
 powershell -c (New-Object Media.SoundPlayer "C:\Windows\Media\Alarm01.wav").PlaySync()
 
 rem Display the message
-msg * "Hourly Reminder: It's time to check the annoying PPF machines!!!"
+msg * "Hourly Reminder: It's time to check this and that and everything!!!"
 
 exit
